59b2aaffb93112d84bcff39e5b28d3e35f9d5fa3
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2021-07-21 12:36:14 +0100

    Added countries additional details to the cards

--- src/components/countries.js
+++ src/components/countries.js
@@ -4,8 +4,11 @@ const Countries = ({countryData}) => {
     return (
         <div>
             {countryData.map((country, index) => (
-                <div key={index}>
-                    <img src = {country.flag} className = "flag"></img>
+                <div key={index} className = "card">
+                    <img src = {country.flag} className = "flag" alt = "flag"></img>
+                    <h5>{country.name}</h5>
+                    <h5>{country.capital}</h5>
+                    <h5>{country.population}</h5>
                 </div>
             ))}
         </div>
